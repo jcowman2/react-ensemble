f2ed26ac946fb041f79828e35616372f1e8fd8bb
jcowman2 <joe.cowman@huskers.unl.edu>
2020-10-15 17:25:59 -0500

    docs: add Lib API docs

--- packages/react-ensemble/src/index.ts
+++ packages/react-ensemble/src/index.ts
@@ -18,7 +18,22 @@ import SimpleControlPanel from "./components/SimpleControlPanel/SimpleControlPan
 
 import * as TrackUtils from "./utils/TrackUtils/trackUtils";
 
-const Lib = { d3Ease, d3Interpolate };
+/** Contains external packages that are used frequently in animations. */
+const Lib = {
+  /**
+   * References the **d3-ease** library of easing functions.
+   *
+   * See the **d3-ease** documentation: https://github.com/d3/d3-ease/blob/master/README.md
+   */
+  d3Ease,
+
+  /**
+   * References the **d3-interpolate** library of interpolation functions.
+   *
+   * See the **d3-interpolate** documentation: https://github.com/d3/d3-interpolate/blob/master/README.md
+   */
+  d3Interpolate
+};
 
 // Modules
 export { Timeline, Controller, SimpleControlPanel, TrackUtils, Lib };
