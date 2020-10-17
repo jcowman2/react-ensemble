4fdd912ca8e0ac7436e3e43cabc88f52525b4183
jcowman2 <joe.cowman@huskers.unl.edu>
2020-10-17 10:32:03 -0500

    docs(website): add Layers guide

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -82,7 +82,7 @@ export default function Home() {
             </AnimationCard>
             <AnimationCard
               body={[
-                "React Ensemble isn’t restricted to a single rendering method—easily wire in Canvas, Konva, and more."
+                "React Ensemble isn’t restricted to a single rendering method. Use the same animation data to draw with HTML/CSS, SVGs, Canvas, and more."
               ]}
               title="Render Whatever"
               sx={{ mb: 4 }}
